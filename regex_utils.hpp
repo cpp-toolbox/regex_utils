db078dfd3bf71401684ee652ad183d2e5c23aca2
ccn <ccn@cuppajoeman.com>
2025-07-25 04:21:56 -0400

    feat: add in various other helpers and constants

--- regex_utils.hpp
+++ regex_utils.hpp
@@ -1,11 +1,16 @@
 #ifndef REGEX_UTILS_HPP
 #define REGEX_UTILS_HPP
 
+// NOTE: need to be pulled out
+#include "../text_utils/text_utils.hpp"
 #include "../meta_utils/meta_types.hpp"
 #include <regex>
 #include <unordered_map>
 
 namespace regex_utils {
+
+inline std::string surround_with(const std::string &s, const std::string &w) { return w + s + w; }
+inline std::string capture(const std::string &s) { return "(" + s + ")"; }
 
 // === Common regex constants ===
 inline const std::string start_of_line = R"(^)";
@@ -13,6 +18,11 @@ inline const std::string end_of_line = R"($)";
 inline const std::string any_char = R"(.)";
 inline const std::string any_char_greedy = R"(.*)";
 inline const std::string any_char_nongreedy = R"(.*?)";
+
+inline const std::string right_parenthesis = R"(\))";
+inline const std::string left_parenthesis = R"(\()";
+
+inline std::string wrap_parentheses(const std::string &s) { return left_parenthesis + s + right_parenthesis; }
 
 inline const std::string optional_ws = R"(\s*)";
 inline const std::string one_or_more_ws = R"(\s+)";
@@ -26,9 +36,23 @@ inline const std::string word_char = R"(\w)";
 inline const std::string word = R"(\w+)";
 inline const std::string identifier = R"([A-Za-z_]\w*)";
 
+inline const std::string optional_ws_comma = surround_with(",", optional_ws);
+
 inline const std::string int_regex = R"(-?\d+)";
 inline const std::string unsigned_int_regex = R"(\d+)";
 inline const std::string float_regex = R"(-?\d+(?:\.\d+)?)";
+inline const std::string captured_float_regex = wrap_parentheses(float_regex);
+
+inline const std::string float_triplet =
+    surround_with(wrap_parentheses(surround_with(
+                      text_utils::join({float_regex, float_regex, float_regex}, optional_ws_comma), optional_ws)),
+                  optional_ws);
+
+inline const std::string captured_float_triplet = surround_with(
+    wrap_parentheses(surround_with(
+        text_utils::join({capture(float_regex), capture(float_regex), capture(float_regex)}, optional_ws_comma),
+        optional_ws)),
+    optional_ws);
 
 inline const std::unordered_map<type, std::string> type_to_regex = {
     {types::INT, int_regex},     {types::SHORT, int_regex},
